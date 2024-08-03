28c0ce3e28e4a43a2f796b2b35709e8532487d47
manasakc055 <110013127+manasakc055@users.noreply.github.com>
2024-08-03 12:19:04 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    toggle = !toggle;
-    let menu = document.querySelector('.nav-list');
-    if (toggle) {
-        menu.style.transform = "translateX(0%)"
-    } else {
-        menu.style.transform = "translateX(-100%)"
-    }
-}
